aeffa5480eff82fd743a35ef06b05525ce2ec85b
Michael Liendo <mtliendo@gmail.com>
2022-09-14 10:32:48 -0500

    setup dis bih

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css'
+import { Flex, View, Button } from '@aws-amplify/ui-react'
+import random from 'lodash.random'
+import { v4 } from 'uuid'
+import { Amplify } from '@aws-amplify/core'
+import * as gen from './generated'
+Amplify.configure(gen.config)
+const channel = 'reactions'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const emojis = {
+		0: '🔥',
+		1: '👍🏽',
+		2: '👩🏽‍🔬',
+		3: '🦸🏽‍♂️',
+	}
+	const handleClick = async (emote) => {
+		const reaction = {
+			id: v4(),
+			icon: emote,
+		}
+		await gen.publish(channel, JSON.stringify(reaction))
+		// setReactionCount([...reactionCount, reaction])
+	}
+	return (
+		<Flex wrap={'wrap'} justifyContent="center">
+			<Button
+				width={'100%'}
+				variation="primary"
+				onClick={() => handleClick('🔥')}
+			>
+				🔥 Fiya
+			</Button>
+
+			<Button
+				width={'100%'}
+				variation="primary"
+				onClick={() => handleClick('👍🏽')}
+			>
+				👍🏽 I'm messing with it
+			</Button>
+			<Button
+				width={'100%'}
+				variation="primary"
+				onClick={() => handleClick('🦦')}
+			>
+				🦦 The Michael
+			</Button>
+			<Button
+				width={'100%'}
+				variation="primary"
+				onClick={() => handleClick('👨🏻‍🔬')}
+			>
+				👨🏻‍🔬 The Gant
+			</Button>
+			<Button
+				width={'100%'}
+				variation="primary"
+				onClick={() => handleClick('👴🏽')}
+			>
+				👴🏽 The Ken
+			</Button>
+		</Flex>
+	)
 }
 
-export default App;
+export default App
---
+++ src/generated.js
@@ -0,0 +1,57 @@
+import { API, graphqlOperation } from 'aws-amplify'
+
+export const config = {
+	aws_appsync_graphqlEndpoint:
+		'https://w6kfwypaejedfo42mcbl4hiuci.appsync-api.us-east-1.amazonaws.com/graphql',
+	aws_appsync_region: 'us-east-1',
+	aws_appsync_authenticationType: 'API_KEY',
+	aws_appsync_apiKey: 'da2-tt66wiwnabdsngzfzkve7w6qt4',
+}
+
+export const subscribeDoc = /* GraphQL */ `
+	subscription Subscribe($name: String!) {
+		subscribe(name: $name) {
+			data
+			name
+		}
+	}
+`
+
+export const publishDoc = /* GraphQL */ `
+	mutation Publish($data: AWSJSON!, $name: String!) {
+		publish(data: $data, name: $name) {
+			data
+			name
+		}
+	}
+`
+
+/**
+ * @param  {string} name the name of the channel
+ * @param  {Object} data the data to publish to the channel
+ */
+export async function publish(name, data) {
+	return await API.graphql(graphqlOperation(publishDoc, { name, data }))
+}
+
+/**
+ * @param  {string} name the name of the channel
+ * @param  {nextCallback} next callback function that will be called with subscription payload data
+ * @param  {function} [error] optional function to handle errors
+ * @returns {Observable} an observable subscription object
+ */
+export function subscribe(name, next, error) {
+	return API.graphql(graphqlOperation(subscribeDoc, { name })).subscribe({
+		next: ({ provider, value }) => {
+			next(value.data.subscribe, provider, value)
+		},
+		error: error || console.log,
+	})
+}
+
+/**
+ * @callback nextCallback
+ * @param {Object} data the subscription response including the `name`, and `data`.
+ * @param {Object} [provider] the provider object
+ * @param {Object} [payload] the entire payload
+ */

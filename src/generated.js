003d3a5c5bff89dfd73fe5fe70fc14f622e6c680
Michael Liendo <mtliendo@gmail.com>
2022-10-19 08:08:54 -0400

    update api key

--- src/generated.js
+++ src/generated.js
@@ -5,7 +5,7 @@ export const config = {
 		'https://w6kfwypaejedfo42mcbl4hiuci.appsync-api.us-east-1.amazonaws.com/graphql',
 	aws_appsync_region: 'us-east-1',
 	aws_appsync_authenticationType: 'API_KEY',
-	aws_appsync_apiKey: 'da2-lv2hl6mvibbybh5gh6ypbwn3we',
+	aws_appsync_apiKey: 'da2-dnr52rdn25brthx3rbynvd42ue',
 }
 
 export const subscribeDoc = /* GraphQL */ `

89b97fa81a2d6b680408524e3a321c6927495952
focusotter <mtliendo@focusotter.com>
2023-04-20 09:49:01 -0400

    get it

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import './App.css'
 import { Flex, Button } from '@aws-amplify/ui-react'
-import { v4 } from 'uuid'
 import { Amplify } from '@aws-amplify/core'
 import * as gen from './generated'
 import { useRef } from 'react'
@@ -8,31 +6,33 @@ Amplify.configure(gen.config)
 function App() {
 	const emojiRef = useRef([
 		{ emoji: '🔥', displayText: '🔥 Fiya' },
-		{ emoji: '👍🏽', displayText: "👍🏽 This is great" },
+		{ emoji: '👍🏽', displayText: '👍🏽 This is great' },
 		{ emoji: '🦦', displayText: '🦦 Focus Ottered in!' },
-		{ emoji: '🏎', displayText: '🏎 Looks fast!' },
+		{ emoji: '🌴', displayText: '🌴 Miami Vibes' },
+		{ emoji: '👀', displayText: '👀 I see you!' },
 		{ emoji: '🌮', displayText: '🌮 Gimme moar!!' },
 	])
 
 	const handleClick = async (emote) => {
 		const reaction = {
-			id: v4(),
 			icon: emote,
 		}
-		const channel = 'reactions'
+		const channel = 'miami'
 		await gen.publish(channel, JSON.stringify(reaction))
 	}
 	return (
-		<Flex wrap={'wrap'} justifyContent="center">
-			{emojiRef.current.map((emojiItem) => (
-				<Button
-					width={'100%'}
-					variation="primary"
-					onClick={() => handleClick(emojiItem.emoji)}
-				>
-					{emojiItem.displayText}
-				</Button>
-			))}
+		<Flex justifyContent="center">
+			<Flex wrap={'wrap'} justifyContent="center" maxWidth={'400px'}>
+				{emojiRef.current.map((emojiItem) => (
+					<Button
+						width={'100%'}
+						variation="primary"
+						onClick={() => handleClick(emojiItem.emoji)}
+					>
+						{emojiItem.displayText}
+					</Button>
+				))}
+			</Flex>
 		</Flex>
 	)
 }
--- src/generated.js
+++ src/generated.js
@@ -2,10 +2,10 @@ import { API, graphqlOperation } from 'aws-amplify'
 
 export const config = {
 	aws_appsync_graphqlEndpoint:
-		'https://h3ni6aklwjalbd642kaqiwgdfy.appsync-api.us-east-1.amazonaws.com/graphql',
+		'https://6anfwmdqt5adtnnsbr2at7tpoa.appsync-api.us-east-1.amazonaws.com/graphql',
 	aws_appsync_region: 'us-east-1',
 	aws_appsync_authenticationType: 'API_KEY',
-	aws_appsync_apiKey: 'da2-fecx2vpif5eotgoy6kraicamfu	',
+	aws_appsync_apiKey: 'da2-allka6nehfefzapdtdxwzmt6za',
 }
 
 export const subscribeDoc = /* GraphQL */ `
